3d05997b835659f1bdf8c8174b7fb694b4330e8c
AndriyChmyrov <andriy.chmyrov@gmail.com>
2022-08-25 16:35:49 +0200

    moved preprocessor definition
    MATLAB_MEX_FILE -> stdafx.h

--- olympusix/stdafx.h
+++ olympusix/stdafx.h
@@ -5,28 +5,31 @@
 
 #pragma once
 #define DLL_EXPORT_SYM __declspec(dllexport)
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define MATLAB_MEX_FILE
 
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
+// Windows and other standard Header Files:
 #include <windows.h>
-#include <mat.h>
 #include <math.h>
 #include <atlstr.h>
 
-
-
-// TODO: reference additional headers your program requires here
+// MATLAB Header Files:
+#include <mat.h>
 #include "mex.h"
+
+// Olympus PortManager Header Files:
 #include "gt.h"
 #include "mdk_if.h"
 #include "mdk_type.h"
 
 
+// MATLAB libraries:
 #pragma comment(lib, "libmx.lib")
 #pragma comment(lib, "libmex.lib")
 #pragma comment(lib, "libmat.lib")
 
 
+// Olympus PortManager libraries:
 //#pragma comment(lib, "msl_pm.lib")
